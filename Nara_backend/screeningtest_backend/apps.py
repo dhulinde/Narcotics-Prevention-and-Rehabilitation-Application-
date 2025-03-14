45508095de5d8e8070a270b7a352ad387505ff98
dhulinde <160626681+dhulinde@users.noreply.github.com>
2025-03-14 20:26:09 +0530

    Created the login and sign up backend, and adjusted the project structure for the backend

---
+++ Nara_backend/screeningtest_backend/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ScreeningtestBackendConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'screeningtest_backend'
---
+++ Nara_backend/treatmentgenerator_backend/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TreatmentgeneratorBackendConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'treatmentgenerator_backend'
